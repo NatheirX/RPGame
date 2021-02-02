e9c7fcaa674fdd70f04478bf2efdf4559a5bd38b
Natheirx <Mustafanatheir1@gmail.com>
2021-02-01 23:04:01 -0500

    player object created

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let player;
+let width = window.innerWidth - 10;
+let height = window.innerHeight - 25;
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(width, height);
+  player = new Player(width / 2, height / 2);
 }
 
 function draw() {
   background(0);
+  ellipseMode(CENTER);
+  player.show();
+}
+
+function Player(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.show = function () {
+    fill(255);
+    noStroke();
+    circle(this.x, this.y, 25);
+  };
 }
