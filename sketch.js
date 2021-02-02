999b18b86a91855485e56741e5eb55720e765c0f
Natheirx <Mustafanatheir1@gmail.com>
2021-02-02 00:21:13 -0500

    drawing grid for battleships

--- sketch.js
+++ sketch.js
@@ -1,24 +1,58 @@
 let player;
 let width = window.innerWidth - 10;
 let height = window.innerHeight - 25;
+let squares;
 function setup() {
   createCanvas(width, height);
   player = new Player(width / 2, height / 2);
+  squares = [];
+  drawgrid();
 }
 
 function draw() {
-  background(0);
+  background(5);
   ellipseMode(CENTER);
-  player.show();
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].show();
+  }
 }
 
 function Player(x, y) {
   this.x = x;
   this.y = y;
+  this.ships = [];
+}
+
+function ship(x, y, length, width) {
+  this.length = length;
+  this.width = width;
+  this.x = x;
+  this.y = y;
+  this.show = function () {};
+}
+
+function drawgrid() {
+  let width = 0;
+  let height = 0;
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let square = new Square(width, height);
+      squares.push(square);
+      width += 50;
+    }
+    height += 50;
+    width = 0;
+  }
+}
+
+function Square(x, y) {
+  this.x = x;
+  this.y = y;
+  this.bombed = false;
 
   this.show = function () {
-    fill(255);
-    noStroke();
-    circle(this.x, this.y, 25);
+    square(x, y, 50);
+    stroke(0);
+    strokeWeight(4);
   };
 }
