e14342413edf850ec6509783b3d0052896639436
Natheirx <Mustafanatheir1@gmail.com>
2021-02-05 20:43:21 -0500

    fixing mouseEvents

--- mouseevents.js
+++ mouseevents.js
@@ -1,7 +1,8 @@
+let justDragged;
 function mousePressed() {
   if (gameState == 1) {
     // place ships
-    player.ships.forEach((ship) => {
+    player.ships.forEach((ship, index) => {
       if (
         ship.x < mouseX &&
         mouseX < ship.x + ship.width * SQUARESIZE &&
@@ -9,10 +10,10 @@ function mousePressed() {
         mouseY < ship.y + ship.length * SQUARESIZE
       ) {
         ship.isDragged = true;
-        print("mouse is pressed");
+        justDragged = index;
       } else {
         ship.isDragged = false;
-        print("mouse isn't pressed");
+        justDragged = null;
       }
     });
   } else if (gameState == 2) {
@@ -44,36 +45,38 @@ function mouseDragged() {
 function mouseReleased() {
   player.ships.forEach((ship) => {
     ship.isDragged = false;
-    print("mouse released!");
-    let leastDistance = Infinity;
-    let targetSquare;
-    player.squares.forEach((square, index) => {
-      let dist = Math.hypot(ship.x - square.x, ship.y - square.y);
-      if (dist < leastDistance) {
-        leastDistance = dist;
-        targetSquare = index;
-      }
-    });
-    console.log(targetSquare);
-    ship.x = player.squares[targetSquare].x;
-    ship.y = player.squares[targetSquare].y;
-    ship.placed = true;
-    if (ship.rotate) {
-      //handle horizontal ships
-      for (i = 0; i < ship.length; i++) {
-        if (ship.width == 2) {
-          player.squares[targetSquare + i + 1].hasShip = true;
-        }
-        player.squares[targetSquare + i].hasShip = true;
-      }
-    } else {
-      //handle vertical ships
-      for (i = 0; i < ship.length; i++) {
-        if (ship.width == 2) {
-          player.squares[targetSquare + i * 10 + 1].hasShip = true;
-        }
-        player.squares[targetSquare + i * 10].hasShip = true;
-      }
+  });
+  let leastDistance = Infinity;
+  let targetSquare;
+  player.squares.forEach((square, index) => {
+    let dist = Math.hypot(
+      player.ships[justDragged].x - square.x,
+      player.ships[justDragged].y - square.y
+    );
+    if (dist < leastDistance) {
+      leastDistance = dist;
+      targetSquare = index;
     }
   });
+  player.ships[justDragged].x = player.squares[targetSquare].x;
+  player.ships[justDragged].y = player.squares[targetSquare].y;
+  player.ships[justDragged].placed = true;
+  if (ship.rotate) {
+    //handle horizontal ships
+    for (i = 0; i < ship.length; i++) {
+      if (ship.width == 2) {
+        player.squares[targetSquare + i + 1].hasShip = true;
+      }
+      player.squares[targetSquare + i].hasShip = true;
+    }
+  } else {
+    //handle vertical ships
+    for (i = 0; i < ship.length; i++) {
+      if (ship.width == 2) {
+        player.squares[targetSquare + i * 10 + 1].hasShip = true;
+      }
+      print(targetSquare);
+      // player.squares[targetSquare + i * 10].hasShip = true;
+    }
+  }
 }
--- player.js
+++ player.js
@@ -1,26 +1,3 @@
-// class Player {
-//   constructor() {
-//     this.ships = [];
-//     this.lives;
-//     this.turn = true;
-//     this.squares = function (x, y) {
-//       let playerSquares;
-//       let width = x;
-//       let height = y;
-//       for (let i = 0; i < 10; i++) {
-//         for (let j = 0; j < 10; j++) {
-//           let square = new Square(width, height);
-//           playerSquares.push(square);
-//           width += SQUARESIZE;
-//         }
-//         height += SQUARESIZE;
-//         width = x;
-//       }
-//       return playerSquares;
-//     };
-//   }
-// }
-
 function Player() {
   this.ships = [];
   this.lives;
